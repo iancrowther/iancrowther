4c998e1157ac1a0d9306185e09a269f3971bb0bf
Ian Crowther <maxlee7_jedi@hotmail.com>
2012-02-07 23:19:01 +0000

    cleanup

---
+++ site/current/scripts/_vti_cnf/scripts.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|08 Dec 2009 21:04:58 -0000
+vti_author:SR|LAPTOP\\Ian Crowther
+vti_modifiedby:SR|LAPTOP\\Ian Crowther
+vti_nexttolasttimemodified:TW|08 Dec 2009 20:59:45 -0000
+vti_timecreated:TR|08 Dec 2009 14:15:58 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|index.html
+vti_syncofs_localhost\\c\:\\users\\ian crowther\\iancrowther/c\:/users/ian crowther/iancrowther:TX|08 Dec 2009 12:57:42 -0000
+vti_syncwith_localhost\\c\:\\users\\ian crowther\\iancrowther/c\:/users/ian crowther/iancrowther:TW|08 Dec 2009 14:15:58 -0000
+vti_cacheddtm:TX|08 Dec 2009 20:59:45 -0000
+vti_filesize:IR|226078
