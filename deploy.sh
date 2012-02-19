c1e9d845cbccbaadbf2974cd37dc7911fd84243d
Ian Crowther <maxlee7_jedi@hotmail.com>
2012-02-19 14:58:39 +0000

    updated script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd /Library/WebServer/Documents/sites/iancrowther/site/new/
+cd site/new/
 pwd
-
 git ftp init -u iancrowther -p - ftp://ftp.freehostia.com/iancrowther.co.uk/
 #git ftp push --user iancrowther --passwd passion ftp://ftp.freehostia.com/iancrowther.co.uk/
