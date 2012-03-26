48a2ace0e62836a26480ab9abe93912d93ae6109
Ian Crowther <maxlee7_jedi@hotmail.com>
2012-03-26 12:58:50 +0100

    local cleanup

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd site/new/
-pwd
-git ftp init -u iancrowther -p - ftp://ftp.freehostia.com/iancrowther.co.uk/
-#git ftp push --user iancrowther --passwd passion ftp://ftp.freehostia.com/iancrowther.co.uk/
